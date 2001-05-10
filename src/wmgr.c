f63722783662e29919ad69b53a72aaa99cf7c806
Bishop Fox <altf4@phx2600.org>
2001-05-10 11:02:12 +0000

    moved declarations to header file

--- src/wmgr.c
+++ src/wmgr.c
@@ -1,6 +1,6 @@
 //==============================================================================
 //
-// wmgr.c
+// wmgr.c -- Build-in window manager.
 //
 // Copyright (C) 2000,2001 Ralph Lowinski <AltF4@freemint.de>
 //------------------------------------------------------------------------------
@@ -21,6 +21,7 @@
 #include "Atom.h"
 #include "Property.h"
 #include "Request.h"
+#include "ICCC.h"
 #include "x_gem.h"
 #include "x_mint.h"
 #include "Xapp.h"
@@ -76,40 +77,6 @@ static CLIENT _WMGR_Client = {
 	NULL, &_WMGR_Table, xFalse, RetainPermanent, 0ul,
 	{{0}}, {{0}}, {{0}}, "  wm"
 };
-
-
-//------------------------------------------------------------------------------
-// Declarations from 'Xutil.h'
-//
-#ifndef _XUTIL_H_
-
-typedef struct {
-	long flags;       // marks which fields in this structure are defined
-	long _x,_y,_w,_h; // obsolete
-	long min_width, min_height;
-	long max_width, max_height;
-	long inc_width, inc_height;
-	struct {
-		long x;   // numerator
-		long y;   // denominator
-	}    min_aspect, max_aspect;
-	long base_width, base_height;   // added by ICCCM version 1
-	long win_gravity;               // added by ICCCM version 1
-} SizeHints;
-
-// flags argument in size hints
-#define USPosition  (1L << 0) // user specified x, y
-#define USSize      (1L << 1) // user specified width, height
-#define PPosition   (1L << 2) // program specified position
-#define PSize       (1L << 3) // program specified size
-#define PMinSize    (1L << 4) // program specified minimum size
-#define PMaxSize    (1L << 5) // program specified maximum size
-#define PResizeInc  (1L << 6) // program specified resize increments
-#define PAspect     (1L << 7) // program specified min and max aspect ratios
-#define PBaseSize   (1L << 8) // program specified base for incrementing
-#define PWinGravity (1L << 9) // program specified window gravity
-
-#endif _XUTIL_H_
 
 
 //==============================================================================
@@ -761,37 +728,6 @@ _Wmgr_WindByPointer (void)
 }
 
 //------------------------------------------------------------------------------
-typedef struct {
-	long flags;   // marks which fields in this structure are defined
-	long input;   // does this application rely on the window manager to
-	              // get keyboard input?
-	int    initial_state;  // see below
-	Pixmap icon_pixmap;    // pixmap to be used as icon
-	Window icon_window;    // window to be used as icon
-	int    icon_x, icon_y; // initial position of icon
-	Pixmap icon_mask;      // icon mask bitmap
-	XID    window_group;   // id of related window group
-	/* this structure may be extended in the future */
-} WmHints;
-
-/* definition for flags of XWMHints */
-
-#define InputHint 		(1L << 0)
-#define StateHint 		(1L << 1)
-#define IconPixmapHint		(1L << 2)
-#define IconWindowHint		(1L << 3)
-#define IconPositionHint 	(1L << 4)
-#define IconMaskHint		(1L << 5)
-#define WindowGroupHint		(1L << 6)
-#define AllHints (InputHint|StateHint|IconPixmapHint|IconWindowHint| \
-IconPositionHint|IconMaskHint|WindowGroupHint)
-#define XUrgencyHint		(1L << 8)
-
-/* definitions for initial window state */
-#define WithdrawnState 0	/* for windows that are not mapped */
-#define NormalState 1	/* most applications want to start this way */
-#define IconicState 3	/* application wants to start as an icon */
-
 static void
 _Wmgr_DrawIcon (WINDOW * wind, GRECT * clip)
 {
@@ -960,6 +896,9 @@ WmgrMessage (short * msg)
 
 		default:
 			printf ("event #%i %X \n", msg[0], MAIN_KeyButMask);
+	
+	} else {
+		printf ("event #%i %X \n", msg[0], MAIN_KeyButMask);
 	}
 	
 	if (inv_save) WindSaveFlush (xFalse);
